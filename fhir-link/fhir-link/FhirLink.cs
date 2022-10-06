abad4ef377df2260418a89f170080cbc2728fe75
Chad Voelker <chad.voelker@microsoft.com>
2022-10-06 13:31:13 -0500

    Debug _fhirClient results

--- fhir-link/fhir-link/FhirLink.cs
+++ fhir-link/fhir-link/FhirLink.cs
@@ -29,37 +29,56 @@ public class FhirLink
         {
             log.LogInformation($"Run started at {DateTime.UtcNow}");
 
-            var q = new SearchParams().Where("link:missing=false").Select("identifier", "link");
+            var q = new SearchParams().Where("link:missing=false").Select("id", "link"); 
 
             log.LogInformation("Querying FHIR for patients with links");
 
-            var find = await _fhirClient.SearchAsync(q);
+            // CRV: The cast here will get rid of non-patients (ideally would be better to filter with the SearchParams)
+            var find = await _fhirClient.SearchAsync<Patient>(q);
 
             var patients = find.Entry.Select(ec => ec.Resource as Patient).ToList();
 
             // need to find correct structure for composite key of the ids or just unique list...?
             var patientPairs = new Dictionary<(string, string), string>();
 
+            // We are getting 5 patients with inconsistent links (only the first and last are paired, all the rest are only replaced-by and have inconsistent ID formats
+            /*
+                D000000001
+                    Replaces WDT0000000016
+                "D000000001-1"
+                    Replaced by D000000001
+                "4be15074-a29b-45b0-a0f9-ebd8157266a9"
+                    Replaced by "a75e08fd-cf79-4396-934c-4b427e71156c"
+                "2fab9a03-c932-4a05-a2ab-343193f72d9c"
+                    Replaced by 12345
+                WDT0000000016
+                    Replaced by WDT000000001
+                */
+
+
             // organize patients into unique patient pairs using dictionary
             foreach (var patient in patients)
             {
                 // only get replaces links
-                foreach (var link in patient.Link.Where(l => l.Type == Patient.LinkType.Replaces))
+                foreach (var link in patient.Link.Where(l => l.Type == Patient.LinkType.ReplacedBy))
                 {
-                    var replacedPatientId = link.Other.Reference.Split('/').LastOrDefault();
+                    var linkedPatientId = link.Other.Reference.Split('/').LastOrDefault();
 
-                    patientPairs.Add((patient.Id, replacedPatientId), null);
+                    patientPairs.Add((patient.Id, linkedPatientId), null);
                 }
             }
 
             log.LogInformation("Building CSV of merged patients");
 
+
+
             var container = _blogStorageClient.GetContainerReference("test");
+            await container.CreateIfNotExistsAsync();
 
             // todo: clean any illegal characters and settle on filename format
-            var blogName = $"merged_patients_{DateTime.UtcNow.ToShortDateString}_{DateTime.UtcNow.ToShortTimeString}.csv";
+            var blobName = $"merged_patients_{DateTime.UtcNow.ToString("yyyyMMdd_HHmmss")}.csv";
 
-            var blob = container.GetBlockBlobReference(blogName);
+            var blob = container.GetBlockBlobReference(blobName);
 
             using CloudBlobStream stream = await blob.OpenWriteAsync();
 
@@ -75,7 +94,7 @@ public class FhirLink
             stream.Flush();
             stream.Close();
 
-            log.LogInformation($"Uploading {blogName} to data lake.");
+            log.LogInformation($"Uploading {blobName} to data lake.");
         }
         catch (Exception exception)
         {
